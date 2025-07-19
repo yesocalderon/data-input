76e21a06aa6609447feed81499fcdf4b536615fe
yesocalderon <yecidcalderon@hotmail.es>
2025-07-18 21:17:56 -0400

    primer commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Overview
+
+This plugin is an example of search & replace feature with settings.
+
+
+## How to use
+
+1. Insert into document some text for searching (in the plugin code, they are in an array arrCodes).
+2. Open the Plugins tab and press "Example of search and replace".
+3. Enter the text you want to replace each field with and press the "Ok" button.
+
+If you need more information about how to use or write your own plugin, please see this https://api.onlyoffice.com/plugin/basic
---
+++ scripts/code.js
@@ -0,0 +1,61 @@
+/**
+ *
+ * by YCT 2025
+ *
+ */
+(function(window, undefined)
+{
+	var nStage   = 0;
+	//the text that will be searched in the document. 
+	var arrCodes = ["@HR_HP_INPUT", "@CITE_INPUT", "@EQUIPO_INPUT"];
+	var arrIds   = ["inputHR", "inputCITE", "inputEQUIPO"];
+
+	function privateReplaceNext()
+	{
+		//properties structure for search and replace
+		var oProperties = {
+			"searchString"  : arrCodes[nStage],
+			"replaceString" : document.getElementById(arrIds[nStage]).value,
+			"matchCase"     : true
+		};
+		//execute method for search and replace
+		window.Asc.plugin.executeMethod("SearchAndReplace", [oProperties]);
+
+		nStage++;
+		if (nStage >= arrCodes.length)
+			window.Asc.plugin.executeCommand("close", "");
+	}
+
+	window.Asc.plugin.init = function()
+	{
+		var oThis = this;
+		document.getElementById("buttonCancel").onclick = function()
+		{
+			this.executeCommand("close", "");
+		};
+
+		document.getElementById("buttonOK").onclick = function()
+		{
+			privateReplaceNext();
+		};
+	};
+
+	window.Asc.plugin.onMethodReturn = function(returnValue)
+	{
+		//event return for completed method
+		var _plugin = window.Asc.plugin;
+		if (_plugin.info.methodName == "SearchAndReplace")
+		{
+			privateReplaceNext();
+		}
+	};
+
+	window.Asc.plugin.button = function(id)
+	{
+		if (-1 === id)
+		{
+			this.executeCommand("close", "");
+		}
+	};
+
+})(window, undefined);
